a5ef8635a44d731c07d7e5d16bfe4ead46a6fdc1
MahmoudGold <lordlegned750@gmail.com>
2021-07-02 14:58:36 +0200

    The project

---
+++ Esite/Esite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@17h^j5!z8v8gl2a7a&wdmk$+jm3l@3+s+h)q9kq17e$6mu851'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
